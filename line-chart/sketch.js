c46a1aeaa01efc2b0387dc9eed6b2a4a8c117a1f
Mathieu Clement <mclement2@dons.usfca.edu>
2018-02-12 21:31:27 -0800

    Values

--- line-chart/sketch.js
+++ line-chart/sketch.js
@@ -37,14 +37,22 @@ function get_years() {
     return uniqueYears;
 }
 
+function round_10_below(n) {
+    return Math.floor(n/10) * 10;
+}
+
+function round_10_above(n) {
+    return Math.ceil(n/10) * 10;
+}
+
 var leftMargin = 100;
 var topMargin = 50;
 
-var plotHeight = 400;
+var plotHeight = 350;
 var plotWidth = 400;
 
 function setup() {
-    createCanvas(1000,1000);
+    createCanvas(1000,600);
 
     var years = get_years();
     console.log('Years', years);
@@ -88,7 +96,60 @@ function setup() {
     rotate(-90);
     textAlign(CENTER);
     text('Average horsepower', 0 - (topMargin + plotHeight/2), leftMargin - 50);
+    rotate(90);
 
+    // Y axis ticks
+    
+    var surplus = 30; // extra pixels on Y axis inside plot for spacing
+
+    // compute average for each year, and min / max
+    var minHp = 1000000;
+    var maxHp = 0;
+
+    var hpAverages = {};
+    for (var i = 0 ; i < years.length ; i++) {
+        var year = years[i];
+        var total = 0;
+        var cars = table.findRows('' + year, 'Year');
+        console.log('Cars for year ' + year, cars);
+
+        for (var j = 0 ; j < cars.length ; j++) {
+            var hp = cars[j].getNum('Horsepower');
+            total = total + hp;
+        }
+
+        var avg = total / cars.length;
+        hpAverages[year] = avg;
+        if (avg < minHp) minHp = avg;
+        if (avg > maxHp) maxHp = avg;
+    }
+    console.log('Averages of horsepower per year', hpAverages);
+    console.log('min avg hp', minHp);
+    console.log('max avg hp', maxHp);
+
+    minHp = round_10_below(minHp);
+    maxHp = round_10_above(maxHp);
+
+    var pixelsPerHp = (plotHeight - surplus * 2) / (maxHp - minHp);
+    console.log('pixelsPerHp', pixelsPerHp);
+
+    // Draw values
+    var xes = []; // x coordinates for each year
+    var yes = []; // y coordinates for each year
+    for (var i = 0 ; i < years.length ; i++) {
+        var year = years[i];
+        var hpAboveMin = hpAverages[year] - minHp;
+        var x = leftMargin + pixelsPerYear * (1+i);
+        var y = topMargin + plotHeight - surplus - pixelsPerHp * hpAboveMin;
+        xes[i] = x;
+        yes[i] = y;
+    }
+    console.log(xes);
+    console.log(yes);
+
+    for (var i = 1; i < years.length ; i++) {
+        line(xes[i-1], yes[i-1], xes[i], yes[i]);
+    }
 }
 
 function draw() {
